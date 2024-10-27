4454baa312da2bdcbb89f32f09ea7cf7be5b8e9a
VipinBisht125 <vipinbisht21bca125.eit@gmail.com>
2024-10-27 17:37:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function toggleParagraph() {
+			const paragraph = document.getElementById('optimization-paragraph');
+			if (paragraph.style.display === 'none' || paragraph.style.display === '') {
+				paragraph.style.display = 'block';
+			} else {
+				paragraph.style.display = 'none';
+			}
+		}
