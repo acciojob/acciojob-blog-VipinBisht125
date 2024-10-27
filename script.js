11289ecb715dfca2ea33f1ef245cdcbf763d7867
VipinBisht125 <vipinbisht21bca125.eit@gmail.com>
2024-10-27 17:54:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-//your JS code here. If required.
-function toggleParagraph() {
-			const paragraph = document.getElementById('optimization-paragraph');
-			if (paragraph.style.display === 'none' || paragraph.style.display === '') {
-				paragraph.style.display = 'block';
-			} else {
-				paragraph.style.display = 'none';
-			}
+// Your JS code here. If required.
+function toggleParagraph(id) {
+	const paragraph = document.getElementById(id);
+	paragraph.style.display = (paragraph.style.display === 'none' || paragraph.style.display === '') ? 'block' : 'none';
+}
+
+
+function toggleParagraph(id) {
+			const paragraph = document.getElementById(id);
+			paragraph.style.display = (paragraph.style.display === 'none' || paragraph.style.display === '') ? 'block' : 'none';
+		}
+
+function toggleParagraph(id) {
+			const paragraph = document.getElementById(id);
+			paragraph.style.display = (paragraph.style.display === 'none' || paragraph.style.display === '') ? 'block' : 'none';
 		}
